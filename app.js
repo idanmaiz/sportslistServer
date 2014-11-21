01ae3d8a3334e5fe750184f2029e46f0dc68385c
maizlits@gmail.com <ubuntu@ip-172-31-46-154.eu-west-1.compute.internal>
2014-11-21 22:27:12 +0000

    Adding Config & Libaries File, Editted app.js to import config and lib files

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-var http = require('http');
-http.createServer(function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hello World\n'); }).listen(3000, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3000/');
+/*********
+  Exports
+**********/
+
+var libs = require('./libs');
+var config = require('./config');
+
+/***********************
+  Logger Initialization
+***********************/
+
+var logger = new (libs.winston.Logger)({
+    transports: [
+      new (libs.winston.transports.Console)({ level:logLevel }),
+      new (libs.winston.transports.File)({ filename: logPath , json:false, level:logLevel})
+    ]
+});
+
+logger.cli();
+logger.info('Log level: %s',logLevel);
+
+/**********************
+ Server Initialization
+***********************/
+
+libs.http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World\n'); }).listen(serverPort, "127.0.0.1");
+
+logger.info('Server started on http://127.0.0.1:%d',serverPort); 
---
+++ libs.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var express = require('express');
+var winston = require('winston');
+
+exports.http = http;
+exports.express = express;
+exports.winston = winston;
