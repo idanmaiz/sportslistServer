3a09b70913430bf756caaefe59b9145859f7c445
IdanMaiz <maizlits@gmail.com>
2014-12-06 16:58:12 +0000

    Removing unwanted files

--- libs.js
+++
@@ -1,8 +0,0 @@
-
-exports.bodyParser = require('body-parser');
-exports.http = require('http');
-exports.express = require('express');
-exports.winston = require('winston');
-exports.db = require('./database.js');
-exports.fs = require('fs');
-exports.https = require('https');
